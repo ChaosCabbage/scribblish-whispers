b69ce0405fea8d22e57f34981be91106c4c99777
Paul Courts <paul.courts@autodesk.com>
2020-04-16 12:45:28 +0100

    Serve the game from any /games/xyz URL

--- server/express/src/main.ts
+++ server/express/src/main.ts
@@ -2,15 +2,27 @@ import express from "express";
 import socketIO from "socket.io";
 import http from "http";
 import { createRoom, joinRoom } from "./rooms";
+import path from "path";
+import fetch from "node-fetch";
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 const port = 5385;
 
+const clientPath = path.join(__dirname, "..", "..", "..", "client", "build");
+
 createRoom("test", io);
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/games/:gameid", (req, res) => {
+  fetch(`http://localhost:${port}/index.html`).then((fetchRes) => {
+    fetchRes.body.pipe(res);
+  });
+});
+
+app.use(express.static(clientPath));
 
 io.on("connection", (socket) => {
   socket.on("join-room", (roomCode: string) => {
